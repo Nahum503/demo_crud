45b8f0ff622f1931ef732c9a2d20c806ae1de4a1
mariobachez <marioalejandrokiss98@gmail.com>
2020-05-17 17:40:57 -0600

    he agregado el codigo de la clase Update

--- demo_crud/src/demo_crud/Update.java
+++ demo_crud/src/demo_crud/Update.java
@@ -5,10 +5,48 @@
  */
 package demo_crud;
 
-/**
- *
- * @author ADMIN
- */
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class Update {
     
+     Update() throws SQLException{
+        Scanner leer = new Scanner(System.in);
+        Estudiante person = new Estudiante();
+        ConexionCRUD utilerias = new ConexionCRUD();
+        System.out.println("<< ACTUALIZAR REGISTROS >>");
+        
+        System.out.println("Ingresar id del registro a modificar: ");
+        person.setIdestudiante(leer.nextInt());
+        
+        //reingresando los datos para actualizar
+        String tablaBuscar = "tb_estudiante";
+        String campoBuscar = "id_estudiante, carnet_estudiante, nom_estudiante, ape_estudiante, edad_estudiante";
+        String condicionBuscar = "id_estudiante = "+person.getIdestudiante();
+        utilerias.desplegarRegistros(tablaBuscar, campoBuscar, condicionBuscar);
+        
+        System.out.println("N° de carnet: ");
+        person.setCarnetestudiante(leer.next());
+        
+        System.out.println("Nombre: ");
+        person.setNomestudiante(leer.next());
+        
+        System.out.println("Apellido: ");
+        person.setApeestudiante(leer.next());
+        
+        System.out.println("Edad: ");
+        person.setEdadestudiante(leer.nextInt());
+        
+        String tabla = "tb_estudiante";
+        String camposValoresNuevos = "carnet_estudiante = '" +person.getCarnetestudiante()+ "', nom_estudiante = '"+person.getNomestudiante()+"', ape_estudiante = '"+person.getApeestudiante()+"', edad_estudiante = '"+
+                person.getEdadestudiante()+"'";
+        
+        utilerias.actualizarEliminarRegistro(tabla, condicionBuscar, camposValoresNuevos);
+        System.out.println("Modificado correctamente!");
+        
+        MenuPrincipal.desplegarMenu();
+    }
 }
+
+
