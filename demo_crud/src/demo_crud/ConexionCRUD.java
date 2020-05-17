2d994222bbc0ca27865c3d94701be87a2be05f04
Nahum503 <nahumpineda1999@gmail.com>
2020-05-17 16:32:21 -0600

    ingrese el codigo a la base de datos

--- demo_crud/src/demo_crud/ConexionCRUD.java
+++ demo_crud/src/demo_crud/ConexionCRUD.java
@@ -4,11 +4,30 @@
  * and open the template in the editor.
  */
 package demo_crud;
-
+import java.sql.*;
 /**
  *
  * @author ADMIN
  */
 public class ConexionCRUD {
+    private final String servidor = "jdbc:mysql://127.0.0.1:3306/db_recurso_humano";
     
+    private final String usuario ="root";
+    
+    private final String clave="";
+    
+    private final String driveConector ="com.mysql.jdbc.Driver";
+    
+    private static Connection conexion;
+    
+    public ConexionCRUD(){
+        try {
+            Class.forName(driveConector);
+            
+            conexion=DriverManager.getConnection(servidor, usuario, clave);
+        } catch (ClassNotFoundException |SQLException e) {
+            System.out.println("Conexión fallida! Error! " + e.getMessage());
+        }
+        
+    }
 }
