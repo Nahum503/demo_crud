bded867f31941136d1852c859cc2984b1e27f201
mariobachez <marioalejandrokiss98@gmail.com>
2020-05-17 17:37:11 -0600

    he agregado el codigo de la clase Read

--- demo_crud/src/demo_crud/Read.java
+++ demo_crud/src/demo_crud/Read.java
@@ -5,10 +5,28 @@
  */
 package demo_crud;
 
-/**
- *
- * @author ADMIN
- */
+import java.sql.SQLException;
+
 public class Read {
-    
+     public Read() throws SQLException{
+        System.out.println("<< CONSULTAR REGISTROS >>");
+        mostrarResultado(); //Llamar metodo dentro de la clase
+        }
+   private void mostrarResultado() throws SQLException{
+        try {
+            ConexionCRUD utilerias = new ConexionCRUD();
+            String tabla = "tb_estudiante";
+            String camposTabla = "*";
+            //condicion se envia vacia para indicar que todos los registros se necesitan mostrar
+            
+            String condicionBusqueda="";
+            //Metodo que realiza la busqueda
+            utilerias.desplegarRegistros(tabla, camposTabla, condicionBusqueda);
+        } catch (Exception ex) {
+            System.out.println("Ha ocurrido un error: "+ex.getMessage());
+        }finally{
+            MenuPrincipal.desplegarMenu();
+        }
+        
+    }
 }
