76f2cf9509d7857a17ddbd0064b5b10c2f946286
Nahum503 <nahumpineda1999@gmail.com>
2020-05-17 17:26:38 -0600

    he agregado el codigo de la clase estudiante

--- demo_crud/src/demo_crud/Estudiante.java
+++ demo_crud/src/demo_crud/Estudiante.java
@@ -7,5 +7,49 @@ package demo_crud;
 
 
 public class Estudiante {
-    
+     private int idestudiante;
+    private String carnetestudiante;
+    private String nomestudiante;
+    private String apeestudiante;
+    private int edadestudiante;
+
+    public int getIdestudiante() {
+        return idestudiante;
+    }
+
+    public void setIdestudiante(int idestudiante) {
+        this.idestudiante = idestudiante;
+    }
+
+    public String getCarnetestudiante() {
+        return carnetestudiante;
+    }
+
+    public void setCarnetestudiante(String carnetestudiante) {
+        this.carnetestudiante = carnetestudiante;
+    }
+
+    public String getNomestudiante() {
+        return nomestudiante;
+    }
+
+    public void setNomestudiante(String nomestudiante) {
+        this.nomestudiante = nomestudiante;
+    }
+
+    public String getApeestudiante() {
+        return apeestudiante;
+    }
+
+    public void setApeestudiante(String apeestudiante) {
+        this.apeestudiante = apeestudiante;
+    }
+
+    public int getEdadestudiante() {
+        return edadestudiante;
+    }
+
+    public void setEdadestudiante(int edadestudiante) {
+        this.edadestudiante = edadestudiante;
+    }
 }
