bac3025b7bf017898be58715a2cbd97c3cc27523
mariobachez <marioalejandrokiss98@gmail.com>
2020-05-17 17:08:54 -0600

    he  agregado el codigo de la clase create

--- demo_crud/src/demo_crud/Create.java
+++ demo_crud/src/demo_crud/Create.java
@@ -5,10 +5,38 @@
  */
 package demo_crud;
 
-/**
- *
- * @author ADMIN
- */
+import java.util.Scanner;
+import java.sql.SQLException;
+
+
 public class Create {
-    
+    Create() throws SQLException{
+        Scanner leer = new Scanner(System.in);
+        Estudiante person = new Estudiante();
+        System.out.println("<< CREAR REGISTRO >>");
+        
+        System.out.println("N° de carnet: ");
+        person.setCarnetestudiante(leer.nextLine());
+        
+        System.out.println("Nombre: ");
+        person.setNomestudiante(leer.nextLine());
+        
+        System.out.println("Apellido: ");
+        person.setApeestudiante(leer.nextLine());
+        
+        System.out.println("Edad: ");
+        person.setEdadestudiante(leer.nextInt());
+        
+        String tabla = "tb_estudiante";
+        String camposTabla = "carnet_estudiante, nom_estudiante, ape_estudiante, edad_estudiante";
+        String valoresCampos = "'"+person.getCarnetestudiante()+"','"+person.getNomestudiante()+"','"+
+                person.getApeestudiante()+"','"+person.getEdadestudiante()+"'";
+        
+        ConexionCRUD utilerias = new ConexionCRUD();
+        
+        utilerias.guardarRegistros(tabla, camposTabla, valoresCampos);
+        
+        MenuPrincipal.desplegarMenu();
+        
+    }
 }
